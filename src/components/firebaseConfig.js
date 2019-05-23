af81f40ba94d4db9678e6d655bff829ca712ab43
Yoshihiro Nakazawa <nooboolean@i.softbank.jp>
2019-05-24 01:00:00 +0900

    Delete firebaseConfig.js

--- src/components/firebaseConfig.js
+++
@@ -1,8 +0,0 @@
-export default {
-  apiKey: "AIzaSyDdF6UCvt23dLdNYY_XthhE-SWQsiycNhc",
-    authDomain: "testfirestore-c9d0a.firebaseapp.com",
-    databaseURL: "https://testfirestore-c9d0a.firebaseio.com",
-    projectId: "testfirestore-c9d0a",
-    storageBucket: "testfirestore-c9d0a.appspot.com",
-    messagingSenderId: "33631851849"
-}
